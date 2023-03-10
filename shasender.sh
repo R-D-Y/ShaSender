5ef7837452d4103ca717241241d1e1e0768002d1
Rémi... et pas Rémy <102509252+R-D-Y@users.noreply.github.com>
2023-03-10 16:22:54 +0100

    Create shasender.sh
    Comments added

---
+++ shasender.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#Made by R-D-Y
+
+
+# Servers list
+mapfile -t servers < /root/scripts/servers.txt
+
+# Log File > you can change with your own path 
+logfile="/root/scripts/scp.log"
+
+function f_preambule () {
+        preambule="$(date +%Y/%m/%d-%T.%3N) - $server"
+}
+
+# Public key
+PUBLIC_KEY=~/.ssh/id_rsa.pub
+
+
+# Loop for each servers
+for server in "${servers[@]}"
+do
+   f_preambule
+
+   if ssh-copy-id -i $PUBLIC_KEY $server
+   then
+           echo $preambule ": Your public key have been sent to $server " >> $logfile
+   else
+           echo $preambule ": Error during sending your public key to $server" >> $logfile
+           echo $preambule ": ShaSender have been stopped" >> $logfile
+   fi
+
+
+  # Write on the log file
+f_preambule
+   echo $preambule" : Le script s'est déroulé correctement" >> $logfile
+   echo "==================================================================================================" >> $logfile
+
+done
+
+echo "==================================================================================================" >> $logfile
